42628037586979104958064543fc9f68ec52e4cd
ln32 <jhchoi602@gmail.com>
2024-08-26 22:37:29 +0900

    24.08.26 - 유틸 툴 초안
    Signed-off-by: 최재훈 <jhchoi602@gmail.com>

--- Assets/Editor/DataHandlerDrawer.cs
+++ Assets/Editor/DataHandlerDrawer.cs
@@ -3,19 +3,22 @@ using UnityEditor;
 
 
 [CustomPropertyDrawer(typeof(NamedArrayAttribute))]
-public class MoneyDataHandlerDrawer : PropertyDrawer
+public class DataHandlerDrawer : PropertyDrawer
 {
+
     public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
     {
         try
         {
             int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
-            EditorGUI.ObjectField(rect, property, new GUIContent(((NamedArrayAttribute)attribute).names[pos]));
+            label = new GUIContent(((NamedArrayAttribute)attribute).names[pos]);
         }
         catch
         {
             EditorGUI.ObjectField(rect, property, label);
         }
+
+        EditorGUI.PropertyField(rect, property, label);
     }
 }
 
