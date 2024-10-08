06d2696951682ce0c9786e6e8e6d3814239f2e76
ln32 <jhchoi602@gmail.com>
2024-10-08 18:08:18 +0900

    Proto 설계 초안
    Signed-off-by: 최재훈 <jhchoi602@gmail.com>

---
+++ Assets/Association/JH/_Proto/Proto_Gacha.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Proto_Gacha : MonoBehaviour
+{
+    public List<ToggleCmdUnit> toggleList = new();
+    private int currentIndex = -1;
+
+    private void Update()
+    {
+        // 마우스 클릭 시 이벤트 실행
+        if (Input.GetMouseButtonDown(0)) // 0은 좌클릭
+        {
+            ExecuteNextEvent();
+        }
+    }
+
+    private void ExecuteNextEvent()
+    {
+        currentIndex = (currentIndex + 1) % toggleList.Count;
+        ChangeView();
+    }
+
+    private void ChangeView()
+    {
+        for (int i = 0; i < toggleList.Count; i++)
+        {
+            toggleList[i].SetActiveFalse_Func();
+        }
+
+        toggleList[currentIndex].InvokeFunc();
+    }
+}
+
+[Serializable]
+public class ToggleCmdUnit
+{
+    public List<GameObject> _goList;
+    public UnityEvent _event;
+
+    public void InvokeFunc()
+    {
+        _event?.Invoke();
+        foreach (var item in _goList)
+        {
+            item.SetActive(true);
+        }
+    }
+
+    public void SetActiveFalse_Func()
+    {
+        foreach (var item in _goList)
+        {
+            item.SetActive(false);
+        }
+    }
+}
