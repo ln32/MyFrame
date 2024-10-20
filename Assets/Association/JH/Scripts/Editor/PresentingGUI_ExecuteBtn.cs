9edb22aa6f258be933eabd5a49e87558a6b2278a
ln32 <jhchoi602@gmail.com>
2024-10-20 16:57:35 +0900

    임시로 apply excute btn 구현, 곧 다듬겠습니다.
    Signed-off-by: 최재훈 <jhchoi602@gmail.com>

---
+++ Assets/Association/JH/Scripts/Editor/PresentingGUI_ExecuteBtn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PresentingTool_Default))]
+public class PresentingGUI_ExecuteBtn : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PresentingTool_Default itemtrigger = (PresentingTool_Default)target;
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button("Execute", GUILayout.Width(100), GUILayout.Height(20)))
+        {
+            itemtrigger.ReFresh();
+        }
+        GUILayout.FlexibleSpace();  // 고정된 여백을 넣습니다.
+        EditorGUILayout.EndHorizontal();  // 가로 생성 끝
+    }
+}
--- Assets/Association/JH/Scripts/ManagingData/Presenter/Sample/PresentingGUI_Default.cs
+++ Assets/Association/JH/Scripts/ManagingData/Presenter/Sample/PresentingGUI_Default.cs
@@ -9,6 +9,8 @@ public class PresentingGUI_Default : MonoBehaviour
     [SerializeField] internal List<ManagningGUI_DataEnum> myGUI_DataEnum;
     internal iManagingDataHandler<DataEnum, int> _handler => DataManager.instance.DataEnum;
 
+    internal List<ManagningGUI_SingleSprite> oldData_ProfileSprite = new();
+    internal List<ManagningGUI_DataEnum> oldData_DataEnum = new();
 
 
     private void Start()
@@ -17,11 +19,42 @@ public class PresentingGUI_Default : MonoBehaviour
         foreach (var item in myGUI_DataEnum)
         {
             _handler.SetObserving(item._dataEnum, item, item.OnInitInvoke);
+            oldData_DataEnum.Add(item);
         }
 
         foreach (var item in myGUI_ProfileSprite)
         {
             DataManager.instance._ProfileData._profileImg.SetObserving(item, item.OnInitInvoke);
+            oldData_ProfileSprite.Add(item);
+        }
+    }
+
+    public void ReFresh()
+    {
+        // OnEnable시, myMember 순회돌며 호출 등록
+        foreach (var item in oldData_DataEnum)
+        {
+            item._OnDisableAction?.Invoke();
+        }
+        foreach (var item in oldData_ProfileSprite)
+        {
+            item._OnDisableAction?.Invoke();
+        }
+
+        oldData_DataEnum.Clear();
+        oldData_ProfileSprite.Clear();
+
+        // Start 시 호출 등록
+        foreach (var item in myGUI_DataEnum)
+        {
+            _handler.SetObserving(item._dataEnum, item, item.OnInitInvoke);
+            oldData_DataEnum.Add(item);
+        }
+
+        foreach (var item in myGUI_ProfileSprite)
+        {
+            DataManager.instance._ProfileData._profileImg.SetObserving(item, item.OnInitInvoke);
+            oldData_ProfileSprite.Add(item);
         }
     }
 
